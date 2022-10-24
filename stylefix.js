bb6660f47c3a9bcd5c61386471afe84e4c3601bd
Andreas Doll <33754407+dollhaus@users.noreply.github.com>
2022-10-24 23:30:46 +0100

    Update stylefix.js

--- stylefix.js
+++ stylefix.js
@@ -1,2 +1,8 @@
+//usage: Add this AFTER the closing </body> tag. This should ensure that it is not executed until the page is loaded.
+
+//VW and VH (respectively, viewport width and viewport height) are far more reliable than percentages when assigning width or height. 
+//They ensure that the value passed is always calculated in respect of the size of the browser window, rather than the containing element. They are 
+//valid just about anywhere, and are very useful in conjunction with flexboxes.
+
 $(".container:first").css({ "width": "80vw", "margin-left": "0", "max-width": "auto !important" });
 $(".table-responsive:first").css("width", "80vw");
